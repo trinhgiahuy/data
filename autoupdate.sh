0cd1cb02b945a65e1fcc0f4af7c0a08595bf46d5
trinhgiahuy <giahuy050201@gmail.com>
2021-07-17 19:35:12 +0300

    : Update files

--- autoupdate.sh
+++ autoupdate.sh
@@ -5,7 +5,7 @@ cd $DATADIR
 
 if [[ -n $(git status -s) ]]; then
     echo "Changed found. Pushing changes..."
-    git add -A && git commit -m "$1: Update files" && git push
+    git add -A && git commit -m "$1: Update files" && git push origin main
 else
     echo "No changes found. Skip pushing."
 fi
